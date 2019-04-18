016802ab22a411e34e7fa3329ab5f797c0f0b2bd
shavkunov <mv.shavkunov@yandex.ru>
2019-04-18 15:03:27 +0300

    changed

--- toloka.js
+++ toloka.js
@@ -3,43 +3,24 @@ exports.Task = extend(TolokaHandlebarsTask, function (options) {
 }, {
   onRender: function() {
     var DOM = this.getDOMElement();
-    var solution = this.getSolution();
-    var task = this.getTask();
-    $(DOM).find("div.topics").click(function() {
-      showSublist(DOM, solution.output_values.chosen_lvl1);
+    var currentList = DOM.querySelectorAll("div.list")[0];
+    currentList.style.backgroundColor = "#E6E6FA";
+    currentList.focus();
+
+    $(DOM).find("div.list").click(function() {
+      var everyList = DOM.querySelectorAll("div.list");
+      for (var i = 0; i < everyList.length; i++) {
+        everyList[i].style.backgroundColor = "#FFFFFF";
+      }
+
+      this.style.backgroundColor = "#E6E6FA";
+      this.focus();
     });
-
-    solution.output_values.news = task.input_values.news;
-    solution.output_values.news_id = task.input_values.news_id;
-    solution.output_values.pred_lvl1 = task.input_values.pred_lvl1;
-    solution.output_values.pred_lvl2 = task.input_values.pred_lvl2;
-    solution.output_values.url = task.input_values.url;
-    solution.output_values.probability = task.input_values.probability;
-
-    if (solution.output_values.chosen_lvl1) {
-      TolokaHandlebarsTask.prototype.setSolution.call(this, solution);
-    }
   },
   onDestroy: function() {
     // Задание завершено, можно освобождать (если были использованы) глобальные ресурсы
   }
 });
-
-function showSublist(DOM, first_item) {
-  var topics = ['спорт', 'политика', 'культура', 'наука_и_техника', 'силовые_структуры', 'бизнес', 'проишествия', 'экономика_и_финансы', 'общество'];
-  var className = 0;
-  for(var i = 0; i < topics.length; i++) {
-    if (topics[i] == first_item) {
-      className = ".key" + i;
-    }
-
-    var selectedList = DOM.querySelector(".key" + i);
-    selectedList.style.display = 'none';            
-  }
-
-  var selectedList1 = DOM.querySelector(className);
-  selectedList1.style.display = 'block';
-}
 
 function extend(ParentClass, constructorFunction, prototypeHash) {
   constructorFunction = constructorFunction || function () {};
