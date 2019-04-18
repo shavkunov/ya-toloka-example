c2b4ccf5bd7873ef71cc19e82fdc933b74365d0b
shavkunov <mv.shavkunov@yandex.ru>
2019-04-18 17:39:59 +0300

    new iteration

--- toloka.js
+++ toloka.js
@@ -1,26 +1,45 @@
 exports.Task = extend(TolokaHandlebarsTask, function (options) {
   TolokaHandlebarsTask.call(this, options);
 }, {
-  onRender: function() {
-    var DOM = this.getDOMElement();
-    var currentList = DOM.querySelectorAll("div.list")[0];
-    currentList.style.backgroundColor = "#E6E6FA";
-    currentList.focus();
+  setSolution: function(solution) {
+      var secondScale = this.getDOMElement().querySelector('.second-scale');
+      var topics = ['спорт', 'политика', 'культура',
+      'наука_и_техника', 'силовые_структуры', 'бизнес',
+      'проишествия', 'экономика_и_финансы', 'общество'];
 
-    $(DOM).find("div.list").click(function() {
-      var everyList = DOM.querySelectorAll("div.list");
-      for (var i = 0; i < everyList.length; i++) {
-        everyList[i].style.backgroundColor = "#FFFFFF";
+      var DOM = this.getDOMElement();
+      //var solution = this.getSolution();
+      //DOM.addEventListener("keydown", function(event){ console.log(event.key); });
+
+      //$(DOM).find("div.rdlist").click(function() {
+      //DOM.querySelector('.rdlist');
+      var chosenTopic = solution.output_values.chosen_lvl1;
+
+      if (chosenTopic) {
+        var listNumber = topics.indexOf(chosenTopic);
+        showList(DOM, topics, listNumber);
       }
-
-      this.style.backgroundColor = "#E6E6FA";
-      this.focus();
-    });
+      //});
+      TolokaHandlebarsTask.prototype.setSolution.call(this, solution);
+  },
+  onRender: function() {
   },
   onDestroy: function() {
     // Задание завершено, можно освобождать (если были использованы) глобальные ресурсы
   }
 });
+
+function showList(DOM, topics, number) {
+  clean(DOM, topics);
+  var list = DOM.querySelector(".list" + number);
+  list.style.display = 'block';
+}
+
+function clean(DOM, topics) {
+  for(var i = 0; i < topics.length; i++) {
+    DOM.querySelector(".list" + i).style.display = 'none';
+  }
+}
 
 function extend(ParentClass, constructorFunction, prototypeHash) {
   constructorFunction = constructorFunction || function () {};
